864d6b48ad7efb3aea53a78c1b33c7eeedaa1a4e
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-09 09:20:16 -0800

    Added client url to cros origen

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -13,7 +13,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000",
+        "http://codepipeline-us-west-2-791209503483.s3-website-us-west-2.amazonaws.com"}, allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -18,7 +18,8 @@ import com.revature.services.PostService;
 
 @RestController
 @RequestMapping("/post")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000",
+        "http://codepipeline-us-west-2-791209503483.s3-website-us-west-2.amazonaws.com" }, allowCredentials = "true")
 public class PostController {
 
 	private final PostService postService;
