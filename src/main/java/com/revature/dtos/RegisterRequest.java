978df69d37d6ff375471a8098be0baf535cab7cc
RyanReedKnight <ryanreedknight@gmail.com>
2023-01-10 13:48:03 -0800

    Added fields to User model and adjusted other Services acordingly

--- src/main/java/com/revature/dtos/RegisterRequest.java
+++ src/main/java/com/revature/dtos/RegisterRequest.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,5 @@ public class RegisterRequest {
     private String password;
     private String firstName;
     private String lastName;
+    private Date birthday;
 }
